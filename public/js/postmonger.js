99ccf65f0e71311553f23865a66c9d5538cd0fd0
SundeepPerupogu <95463949+SundeepPerupogu@users.noreply.github.com>
2024-05-20 15:50:07 +0530

    Update postmonger.js

--- public/js/postmonger.js
+++ public/js/postmonger.js
@@ -1,1 +1,30 @@
-
+// public/js/postmonger.js
+(function(global) {
+    var Postmonger = global.Postmonger = {};
+    Postmonger.version = '0.0.14';
+    Postmonger.Session = function() {
+        // Implement your Postmonger session handling here
+        var events = {};
+        return {
+            on: function(name, callback) {
+                events[name] = events[name] || [];
+                events[name].push(callback);
+            },
+            off: function(name, callback) {
+                if (events[name]) {
+                    var idx = events[name].indexOf(callback);
+                    if (idx > -1) {
+                        events[name].splice(idx, 1);
+                    }
+                }
+            },
+            trigger: function(name, data) {
+                if (events[name]) {
+                    events[name].forEach(function(callback) {
+                        callback(data);
+                    });
+                }
+            }
+        };
+    };
+})(this);
